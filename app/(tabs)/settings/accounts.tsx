7b7476ed29579c7239c523102584b3d74827779b
toastybridge <jtan208295@gmail.com>
2025-11-15 05:50:15 -0600

    Feat: Implement alerts and accounts subpages for settings

--- app/(tabs)/settings/accounts.tsx
+++ app/(tabs)/settings/accounts.tsx
@@ -1,10 +1,54 @@
+import Divider from '@/components/ui/divider';
 import { GlobalStyles } from '@/constants/theme';
 import { router } from 'expo-router';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function AccountsScreen() {
+
+  const info = [
+    {
+      name: 'SSN',
+      label: '***-**-1234'
+    },
+    {
+      name: 'Email',
+      label: 's****95@gmail.com'
+    },
+    {
+      name: 'Phone Number',
+      label: '***-***-1234'
+    },
+    {
+      name: 'First Name',
+      label: 'Jimothy'
+    },
+    {
+      name: 'Last Name',
+      label: 'John'
+    }
+  ];
+
+  const socials = [
+    {
+      name: 'Instagram',
+      label: '@_account1_'
+    },
+    {
+      name: 'Instagram',
+      label: '@private_account2020_'
+    },
+    {
+      name: 'LinkedIn',
+      label: '/jordan-tan'
+    },
+    {
+      name: 'Reddit',
+      label: 'u/jordan-tan'
+    }
+  ];
+
   return (
     <SafeAreaView style={styles.safeContainer} edges={['top']}>
       <View style={styles.container}>
@@ -15,9 +59,55 @@ export default function AccountsScreen() {
         >
           <Text style={[GlobalStyles.body, { color: "#383AB2" }]}>← Go Back</Text>
         </TouchableOpacity>
+
+        <Divider />
+        <Text style={[GlobalStyles.body, { fontWeight: 'bold' }]}>
+          Connected Info
+        </Text>
+
+        {info.map(item =>
+          <InfoToggle name={item.name} label={item.label} key={item.label} /> // Bad practice but this is not a dynamic list so wtv
+        )}
+
+        <Divider />
+        <Text style={[GlobalStyles.body, { fontWeight: 'bold' }]}>
+          What socials are you fine with sharing?
+        </Text>
+
+        {socials.map(social =>
+          <InfoToggle name={social.name} label={social.label} key={social.label} /> // Bad practice but this is not a dynamic list so wtv
+        )}
       </View>
-    </SafeAreaView>
+
+    </SafeAreaView >
   )
+}
+
+function InfoToggle({ name, label }: { name: string, label?: string }) {
+  const [toggled, setToggled] = useState(true);
+
+  return (
+    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', minWidth: '100%', paddingTop: 8, paddingBottom: 8 }}>
+      <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <Text style={[GlobalStyles.body]}>
+          {name}
+        </Text>
+        {label && toggled &&
+          <Text style={[GlobalStyles.small, { color: "#6C6C6C" }]} >
+            {label}
+          </Text>
+        }
+      </View>
+      <TouchableOpacity
+        style={{ padding: 4 }}
+        onPress={() => setToggled(!toggled)}
+      >
+        <Text style={[GlobalStyles.small, {color: toggled ? "#EF1111" : "#6C6C6C"}]}>
+          {toggled ? "Remove" : "Add"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
--- app/(tabs)/settings/alerts.tsx
+++ app/(tabs)/settings/alerts.tsx
@@ -1,3 +1,5 @@
+import Divider from '@/components/ui/divider';
+import SettingsToggle from '@/components/ui/settings-toggle';
 import { GlobalStyles } from '@/constants/theme';
 import { router } from 'expo-router';
 import React from 'react';
@@ -5,6 +7,19 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function AlertsScreen() {
+  const info = [
+    'SSN',
+    'Email',
+    'Phone Number',
+    'First Name',
+    'Last Name',
+    'Social Media'
+  ]
+  const alertLocs = [
+    'Email',
+    'Phone Number'
+  ]
+
   return (
     <SafeAreaView style={styles.safeContainer} edges={['top']}>
       <View style={styles.container}>
@@ -15,8 +30,27 @@ export default function AlertsScreen() {
         >
           <Text style={[GlobalStyles.body, { color: "#383AB2" }]}>← Go Back</Text>
         </TouchableOpacity>
+
+        <Divider />
+        <Text style={[GlobalStyles.body, { fontWeight: 'bold' }]}>
+          What info do you want alerts for if your info is leaked or used without your consent?
+        </Text>
+
+        {info.map(infoPiece =>
+          <SettingsToggle name={infoPiece} key={infoPiece} /> // Bad practice but this is not a dynamic list so wtv
+        )}
+
+        <Divider />
+        <Text style={[GlobalStyles.body, { fontWeight: 'bold' }]}>
+          What socials are you fine with sharing?
+        </Text>
+
+        {alertLocs.map(loc =>
+          <SettingsToggle name={loc} key={loc} /> // Bad practice but this is not a dynamic list so wtv
+        )}
       </View>
-    </SafeAreaView>
+
+    </SafeAreaView >
   )
 }
 
